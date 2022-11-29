7bb4723c9eade306937994f367da518cf82e1438
flux <25628292+fluxionary@users.noreply.github.com>
2022-11-29 11:51:37 -0800

    fmod and normalization

--- init.lua
+++ init.lua
@@ -1,30 +1,6 @@
-local modname = minetest.get_current_modname()
-local modpath = minetest.get_modpath(modname)
-local S = minetest.get_translator(modname)
+futil.check_version({ year = 2022, month = 10, day = 24 })
 
-assert(
-	type(futil.version) == "number" and futil.version >= os.time({ year = 2022, month = 10, day = 24 }),
-	"please update futil"
-)
-
-craftsystem = {
-	version = os.time({ year = 2022, month = 11, day = 14 }),
-	fork = "fluxionary",
-
-	modname = modname,
-	modpath = modpath,
-	S = S,
-
-	has = {},
-
-	log = function(level, messagefmt, ...)
-		return minetest.log(level, ("[%s] %s"):format(modname, messagefmt:format(...)))
-	end,
-
-	dofile = function(...)
-		return dofile(table.concat({ modpath, ... }, DIR_DELIM) .. ".lua")
-	end,
-}
+craftsystem = fmod.create()
 
 craftsystem.dofile("api")
 craftsystem.dofile("do_registration")
